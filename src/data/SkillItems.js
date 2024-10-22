09ff8228fbdaf2a9fc5de6b3e732c74989806e10
Wil Hargrave <willhargrave@hotmail.co.uk>
2024-10-22 14:56:43 +0100

    updated skills

--- src/data/SkillItems.js
+++ src/data/SkillItems.js
@@ -3,12 +3,17 @@ const SkillItems = [
     {id: uniqid(), name: 'Ruby', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg"},
     {id: uniqid(), name: 'React', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"},
     {id: uniqid(), name: 'Javascript', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"},
-    {id: uniqid(), name: 'HTML', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"},
-    {id: uniqid(), name: 'CSS', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},
     {id: uniqid(), name: 'SQL', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"},
     {id: uniqid(), name: 'Node', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"},
-    {id: uniqid(), name: 'Heroku', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg"},
     {id: uniqid(), name: 'Mongo', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"},
+    {id: uniqid(), name: 'Go', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg"},
+    {id: uniqid(), name: 'Java', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"},
+    {id: uniqid(), name: 'Nextjs', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"},
+    {id: uniqid(), name: 'Typescript', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"},
+    {id: uniqid(), name: 'Python', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"},
+    {id: uniqid(), name: 'Azure', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg"},
+    {id: uniqid(), name: 'AWS', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"},
+    {id: uniqid(), name: 'Docker', img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"},
 ]
 
 export default SkillItems;
